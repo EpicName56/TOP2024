c5c15b7267a5d979c5192e4dfc60405a9519347d
EpicName56 <vladmont96@yandex.ru>
2024-09-01 12:40:07 +0500

    Задание по 5 уроку

---
+++ src/Lesson_5.java
@@ -0,0 +1,19 @@
+public class Lesson_5 {
+    public static void main(String[] args) {
+
+        int firstInt = 1;
+        byte firstByte = 2;
+        short firstShort = 3;
+        long firstLong = 4L;
+
+        System.out.println();
+        System.out.println("int:" +firstInt);
+        System.out.println(firstByte);
+        System.out.println(firstShort);
+        System.out.println(firstLong);
+        System.out.println();
+
+        char firstChar = 'a';
+        System.out.println(firstChar);
+    }
+}
