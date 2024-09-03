c4b4722f528599d1cf2af5108bcf7290213d492a
EpicName56 <vladmont96@yandex.ru>
2024-09-03 12:16:07 +0500

    Задание по 7-8 урокам

--- src/Lesson_7.java
+++ src/Lesson_7.java
@@ -1,38 +1,18 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Lesson_7 {
 
     public static void main(String[] args) {
 
-        byte firstByte = 1;
-        short firstShort = 2;
-        int firstInt = 3;
-        long firstLong = 4L;
-        float firstFloat = 5;
-        double firstDouble = 6;
-        char firstChar = 'A';
-        boolean firstBoolean = true;
+        Scanner firstScan = new Scanner(System.in);
 
-        System.out.println("byte: " + firstByte);
-        System.out.println("short: " + firstShort);
-        System.out.println("int: " + firstInt);
-        System.out.println("long: " + firstLong);
-        System.out.println("float: " + firstFloat);
-        System.out.println("double: " + firstDouble);
-        System.out.println("char: " + firstChar);
-        System.out.println("boolean: " + firstBoolean);
+        System.out.print("Введите первое число: ");
+        int number = firstScan.nextInt();
 
-        System.out.println();
+        System.out.print("Введите второе число: ");
+        int secondNumber = firstScan.nextInt();
 
-        System.out.println("Сложение: " + (firstByte + firstShort));
-        System.out.println("Умножение: " + (firstInt * firstLong));
-
-        Scanner newScanner = new Scanner(System.in);
-
-        Random newRandom = new Random();
-        char secondChar = (char) newRandom.nextInt(1, 4);
-        System.out.println();
-        System.out.println(secondChar);
+        System.out.print("Сумма введенных чисел: ");
+        System.out.print(number + secondNumber);
     }
 }
---
+++ src/Lesson_8.java
@@ -0,0 +1,64 @@
+import java.util.Random;
+
+public class Lesson_8 {
+
+    public static void main(String[] args) {
+
+        byte firstByte = 1;
+        short firstShort = 2;
+        int firstInt = 3;
+        long firstLong = 4L;
+        float firstFloat = 5;
+        double firstDouble = 6;
+        char firstChar = 'A';
+        boolean firstBoolean = true;
+
+        System.out.println("byte: " + firstByte);
+        System.out.println("short: " + firstShort);
+        System.out.println("int: " + firstInt);
+        System.out.println("long: " + firstLong);
+        System.out.println("float: " + firstFloat);
+        System.out.println("double: " + firstDouble);
+        System.out.println("char: " + firstChar);
+        System.out.println("boolean: " + firstBoolean);
+
+        System.out.println();
+
+        System.out.println("Сложение: " + (firstByte + firstShort));
+        System.out.println("Умножение: " + (firstInt * firstLong));
+
+        // скорее всего, это все можно уместить в одну или две строки и даже не обязательно писать четыре рандома //
+        // но я не ищу легких путей, я по ним иду //
+
+        Random firstRandom = new Random();
+
+        int testRandom = (char) firstRandom.nextInt(0, 65535);
+        int secondTestRandom = (char) firstRandom.nextInt(0, 65535);
+        int thirdTestRandom = (char) firstRandom.nextInt(0, 65535);
+        int fourTestRandom = (char) firstRandom.nextInt(0, 65535);
+
+        char randomChar = (char) (testRandom);
+        char secondRandomChar = (char) (secondTestRandom);
+        char thirdRandomChar = (char) (thirdTestRandom);
+        char fourRandomChar = (char) (fourTestRandom);
+
+        System.out.print("Символы: ");
+        System.out.print(randomChar);
+        System.out.print(secondRandomChar);
+        System.out.print(thirdRandomChar);
+        System.out.print(fourRandomChar);
+
+        System.out.println();
+
+        // я не понимамю, как я это делаю, но это, вроде, работает :D //
+
+        char secondCharTransform = (char) (firstChar + 1);
+        char thirdCharTransform = (char) (firstChar + 2);
+        char fourCharTransform = (char) (firstChar + 3);
+
+        System.out.print(firstChar);
+        System.out.print(secondCharTransform);
+        System.out.print(thirdCharTransform);
+        System.out.print(fourCharTransform);
+    }
+}
