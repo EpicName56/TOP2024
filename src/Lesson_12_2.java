37e0d52e9e4b8d69afac37fc14db27929e07c5fe
EpicName56 <vladmont96@yandex.ru>
2024-09-13 14:58:22 +0500

    Задание по 11-12 урокам

--- src/Lesson_12_2.java
+++ src/Lesson_12_2.java
@@ -16,12 +16,14 @@ public class Lesson_12_2 {
         int finalSalaryMaria = salaryMaria + newSalaryMaria;
 
         System.out.println("Диана теперь получает " + finalSalaryDiana +
-                " рублей. Годовой доход Дианы вырос на " + newSalaryDiana + " рублей");
+                " рублей в месяц. Годовой доход Дианы вырос на " + newSalaryDiana * 12 + " рублей");
 
-        System.out.println("Андрей теперь получает " + finalSalaryAndrey + " рублей. Годовой доход Андрея вырос" +
-                " на " + newSalaryAndrey + " рублей");
+        System.out.println("Андрей теперь получает " + finalSalaryAndrey +
+                " рублей в месяц. Годовой доход Андрея вырос" +
+                " на " + newSalaryAndrey * 12 + " рублей");
 
-        System.out.println("Мария теперь получает " + finalSalaryMaria + " рублей. Годовой доход Марии вырос на " +
-                newSalaryMaria + " рублей");
+        System.out.println("Мария теперь получает " + finalSalaryMaria +
+                " рублей в месяц. Годовой доход Марии вырос на " +
+                newSalaryMaria * 12 + " рублей");
     }
 }
