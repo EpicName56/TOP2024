e68ac07dedc570471527cbfd73c6f78966f31b35
EpicName56 <vladmont96@yandex.ru>
2024-09-13 15:05:28 +0500

    Задание по 11-12 урокам

--- src/Lesson_12_2.java
+++ src/Lesson_12_2.java
@@ -2,28 +2,32 @@ public class Lesson_12_2 {
 
     public static void main(String[] args) {
 
-        int salaryDiana = 66660;
-        int salaryAndrey = 77770;
-        int salaryMaria = 88880;
+        int salaryDiana = 66_660;
+        int salaryAndrey = 77_770;
+        int salaryMaria = 88_880;
 
-        int newSalaryDiana = (66660 * 15) / 100;
+        int newSalaryDiana = (66_660 * 15) / 100;
         int finalSalaryDiana = salaryDiana + newSalaryDiana;
 
-        int newSalaryAndrey = (77770 * 15) / 100;
+        int newSalaryAndrey = (77_770 * 15) / 100;
         int finalSalaryAndrey = salaryAndrey + newSalaryAndrey;
 
-        int newSalaryMaria = (88880 * 15) / 100;
+        int newSalaryMaria = (88_880 * 15) / 100;
         int finalSalaryMaria = salaryMaria + newSalaryMaria;
 
         System.out.println("Диана теперь получает " + finalSalaryDiana +
-                " рублей в месяц. Годовой доход Дианы вырос на " + newSalaryDiana * 12 + " рублей");
+                " рублей в месяц. Годовой доход Дианы вырос на " + newSalaryDiana * 12 +
+                " рублей. Теперь годовой доход Дианы составляет: " + (salaryDiana + newSalaryDiana) * 12 +
+                " рублей в год");
 
         System.out.println("Андрей теперь получает " + finalSalaryAndrey +
                 " рублей в месяц. Годовой доход Андрея вырос" +
-                " на " + newSalaryAndrey * 12 + " рублей");
+                " на " + newSalaryAndrey * 12 + " рублей. Теперь годовой доход Андрея составляет: " +
+                (salaryAndrey + newSalaryAndrey) * 12 + " рублей в год");
 
         System.out.println("Мария теперь получает " + finalSalaryMaria +
                 " рублей в месяц. Годовой доход Марии вырос на " +
-                newSalaryMaria * 12 + " рублей");
+                newSalaryMaria * 12 + " рублей. Теперь годовой доход Марии составляет: "
+                + (salaryMaria + newSalaryMaria) * 12 + " рублей в год");
     }
 }
