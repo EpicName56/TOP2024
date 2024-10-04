0d3b95cf644208cfc9997bae6eacccd55e9fd8dd
EpicName56 <vladmont96@yandex.ru>
2024-10-04 20:59:02 +0500

    Новое домашнее задание

--- src/Homework_21_22_23_24/Homework_21_22_23_24.java
+++ src/Homework_21_22_23_24/Homework_21_22_23_24.java
@@ -7,9 +7,7 @@ public class Homework_21_22_23_24 {
 
     public static void main(String[] args) {
 
-        int i1 = 5;
-
-        System.out.println(square(i1));
+        System.out.println(square(8));
         System.out.println(stringFullName("Василий", "Васильев"));
         System.out.println(cornerValuesArray('S', 'q', 'H', 'c', 'W'));
         System.out.println(cornerValuesArray(123, 41, 654, 53234, 12, 5512, 64349));
