817f1b9b8f0dc22696a7a6f89703ac97b032de7e
EpicName56 <vladmont96@yandex.ru>
2024-12-04 21:34:36 +0500

    Новое домашнее задание

--- src/Homework_55_56/ArithmeticException.java
+++ src/Homework_55_56/ArithmeticException.java
@@ -2,6 +2,13 @@ package Homework_55_56;
 
 public class ArithmeticException {
 
+    /**
+     * Метод делит первое число на второе
+     *
+     * @param firstNumber  хранит в себе первое число
+     * @param secondNumber хранит в себе второе число
+     * @return возвращает результат деления и обходит ошибку типа ArithmeticException, если она присутствует
+     */
     public static String arithmeticException(int firstNumber, int secondNumber) {
 
         try {
